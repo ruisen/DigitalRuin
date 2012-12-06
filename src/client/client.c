aba12c314325f721ca1b5d7c8836f1fa30d77d30
Ruisen Luo <hong1047@gmail.com>
2012-12-06 16:53:32 +0800

    Test commit again...

--- src/client/client.c
+++ src/client/client.c
@@ -9,46 +9,48 @@
 
 int main(int argc, char *argv[ ])
 {
-  int sockfd;
+    int sockfd;
 
-  /* will hold the destination addr */
-  struct sockaddr_in dest_addr;
+    /* will hold the destination addr */
+    struct sockaddr_in dest_addr;
   
-  printf("I'm a client!\n");
+    printf("I'm a client!\n");
 
-  /* get a socket file descriptor */
-  if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-  {
-    perror("Client-socket() error lol!");
-    exit(1);
-  }
-  else
-  {
-    printf("Client-socket() sockfd is OK...\n");
-  }
+    /* get a socket file descriptor */
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+	perror("Client-socket() error lol!");
+	exit(1);
+    }
+    else
+    {
+	printf("Client-socket() sockfd is OK...\n");
+    }
+    
+    /* host byte order */
+    dest_addr.sin_family = AF_INET;
 
-  /* host byte order */
-  dest_addr.sin_family = AF_INET;
+    /* short, network byte order */
+    dest_addr.sin_port = htons(DEST_PORT);
+    dest_addr.sin_addr.s_addr = inet_addr(DEST_IP);
 
-  /* short, network byte order */
-  dest_addr.sin_port = htons(DEST_PORT);
-  dest_addr.sin_addr.s_addr = inet_addr(DEST_IP);
+    /* zero the rest of the struct */
+    memset(&(dest_addr.sin_zero), 0, 8);
 
-  /* zero the rest of the struct */
-  memset(&(dest_addr.sin_zero), 0, 8);
+    /* connect to server */
+    if(connect(sockfd, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr)) == -1)
+    {
+	perror("Client-connect() error lol");
+	exit(1);
+    }
+    else
+    {
+	printf("Client-connect() is OK...\n");
+    } 
 
-  /* connect to server */
-  if(connect(sockfd, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr)) == -1)
-  {
-    perror("Client-connect() error lol");
-    exit(1);
-  }
-  else
-  {
-    printf("Client-connect() is OK...\n");
-  } 
+    
+    
+    /*...other codes...*/
 
-  /*...other codes...*/
-
-  return 0;
+    return 0;
 }
--- src/server/server.c
+++ src/server/server.c
@@ -1,49 +1,85 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+/* the port users will be connecting to */
 #define MYPORT 8888
+/* how many pending connections queue will hold */
+#define BACKLOG 10
 
 void main()
 {
-  int sockfd;			/* socket file descriptor */
-  struct sockaddr_in my_addr;
+    /* listen on sock_fd, new connection on new_fd */
+    int sockfd, new_fd;
+    /* my address information, address where I run this program */
+    struct sockaddr_in my_addr;
+    /* remote address information */
+    struct sockaddr_in their_addr;
+    int sin_size;
 
-  printf("I'm a server...\n");
+    printf("I'm a server...\n");
   
-  /* get a socket file descriptor */
-  if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-  {
-    perror("Server-socket() error lol!");
-    exit(1);
-  }
-  else
-  {
-    printf("Server-socket() sockfd is OK...\n");
-  }
+    /* get a socket file descriptor */
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+	perror("Server-socket() error lol!");
+	exit(1);
+    }
+    else
+    {
+	printf("Server-socket() sockfd is OK...\n");
+    }
 
-  /* host byte order */
-  my_addr.sin_family = AF_INET;
-  /* short nerwork byte order */
-  my_addr.sin_port = htons(MYPORT);
-  my_addr.sin_addr.s_addr = INADDR_ANY;
-  /* zero the rest of the struct */
-  memset(&(my_addr.sin_zero), 0, 8);
+    /* host byte order */
+    my_addr.sin_family = AF_INET;
+    /* short nerwork byte order */
+    my_addr.sin_port = htons(MYPORT);
+    my_addr.sin_addr.s_addr = INADDR_ANY;
+    /* zero the rest of the struct */
+    memset(&(my_addr.sin_zero), 0, 8);
+    
+    /* bind a port */
+    if(bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1)
+    {
+	perror("Server-bind() error lol!");
+	exit(1);
+    }
+    else
+    {
+	printf("Server-bind() sockfd is OK...\n");
+    }
+    
+    /* listen */
+    if(listen(sockfd, BACKLOG) == -1)
+    {
+	perror("listen() error lol!");
+	exit(1);
+    }
+    else
+    {
+	printf("listen() is Ok...\n");
+    }
 
-  /* bind a port */
-  if(bind(sockfd, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) == -1)
-  {
-    perror("Server-bind() error lol!");
-    exit(1);
-  }
-  else
-  {
-    printf("Server-bind() sockfd is OK...\n");
-  }
+    /* ...other codes to read the received data... */
+    sin_size = sizeof(struct sockaddr_in);
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+    if(new_fd == -1)
+    {
+	perror("accept() error lol!");
+    }
+    else
+    {
+	printf("accept() is OK...\n");
+    }
+	    
+    /* ...other codes... */
 
-  /* ...other codes... */
+    /* close file descriptors */
+    close(new_fd);
+    close(sockfd);
 
-  return 0;
+    return 0;
 }
